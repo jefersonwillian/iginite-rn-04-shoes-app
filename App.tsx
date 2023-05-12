975d74fe79a829d4c7c661dd34b675e4c4b9279f
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-12 15:19:50 -0300

    OneSignal - Instalando o OneSignal

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setAppId('7486666a-de69-47a2-a23b-f13b44d8e0f4');
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
