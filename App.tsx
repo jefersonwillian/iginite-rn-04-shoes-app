3cc0bf1c2e87259c9e9f2029d6af304b7e5ed868
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-15 15:20:25 -0300

    Push Notifications - Removendo Tag

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -10,11 +10,20 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 import OneSignal from 'react-native-onesignal';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
-OneSignal.setAppId('7486666a-de69-47a2-a23b-f13b44d8e0f4');
+
+const oneSignalAppId = Platform.OS === 'ios' ? '' : '7486666a-de69-47a2-a23b-f13b44d8e0f4';
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log(response);
+})
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('rennan.douglas@rocketseat.team');
 
   return (
     <NativeBaseProvider theme={THEME}>
---
+++ src/notifications/notificationsTags.ts
@@ -0,0 +1,6 @@
+import OneSignal from "react-native-onesignal";
+
+export function tagUserEmailCreate(email: string) {
+    // OneSignal.sendTag("user_email", email);
+    OneSignal.deleteTag("user_email");
+}
