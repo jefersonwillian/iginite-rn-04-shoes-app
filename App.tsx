870f066e6bd2337e1b7f86021a09a0ec6c644d11
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-15 16:26:51 -0300

    Push Notifications - Recebendo mensagem em foreground

--- App.tsx
+++ App.tsx
@@ -9,21 +9,32 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-import OneSignal from 'react-native-onesignal';
-import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 
 const oneSignalAppId = Platform.OS === 'ios' ? '' : '7486666a-de69-47a2-a23b-f13b44d8e0f4';
 OneSignal.setAppId(oneSignalAppId);
 
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response);
-})
+// OneSignal.promptForPushNotificationsWithUserResponse(response => {
+//   console.log(response);
+// })
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  tagUserEmailCreate('rennan.douglas@rocketseat.team');
+  tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal
+      .setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
+        console.log(notificationRecivedEvent);
+      })
+
+    return () => unsubscribe;
+
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
