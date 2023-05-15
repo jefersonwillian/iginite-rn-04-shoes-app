3abee7cc0d9089e39ca40136f17c45413857fa34
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-15 16:50:16 -0300

    Push Notifications - Refatorando o componente de Notificação

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
-import { Platform, StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Platform, StatusBar } from 'react-native';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
-import { useEffect, useState } from 'react';
-import { Notification } from './src/components/Notification';
 
 const oneSignalAppId = Platform.OS === 'ios' ? '' : '7486666a-de69-47a2-a23b-f13b44d8e0f4';
 OneSignal.setAppId(oneSignalAppId);
@@ -23,22 +21,8 @@ OneSignal.setAppId(oneSignalAppId);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  const [notification, setNotification] = useState<OSNotification>();
   
   tagUserInfoCreate();
-
-  useEffect(() => {
-    const unsubscribe = OneSignal
-      .setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
-        console.log(notificationRecivedEvent);
-        const response = notificationRecivedEvent.getNotification();
-
-        setNotification(response);
-      })
-
-    return () => unsubscribe;
-
-  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -50,14 +34,6 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-
-      {
-        notification?.title &&
-        <Notification
-          title={notification.title}
-          onClose={() => setNotification(undefined)}
-        />
-      }
     </NativeBaseProvider>
   );
 }
