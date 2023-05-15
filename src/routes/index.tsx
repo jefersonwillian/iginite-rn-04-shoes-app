cb2c3042641715af2b560079d98606fe23f697fa
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2023-05-15 20:03:33 -0300

    Deep Linking - Passando parâmetro no Deep Linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,36 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
 import { Notification } from '../components/Notification';
+
+import * as Linking from 'expo-linking';
+
+const linking = {
+  prefixes: ['igniteshoesapp://', 'com.jeferson.igniteshoes://'],
+  config: {
+    screens: {
+      details: {
+        path: 'details/:productId',
+        parse: {
+          productId: (productId: string) => productId
+        }
+      }
+    }
+  }
+}
+
+
 export function Routes() {
   const [notification, setNotification] = useState<OSNotification>();
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  const deeoLinking = Linking.createURL('details', {
+    queryParams: {
+      productId: '7'
+  }})
+  console.log("🚀 ~ file: index.tsx:37 ~ Routes ~ deeoLinking:", deeoLinking)
 
   useEffect(() => {
     const unsubscribe = OneSignal
